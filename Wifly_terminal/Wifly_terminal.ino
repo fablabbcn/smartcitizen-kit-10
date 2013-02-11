e205d54ca2812ae4148f3f99c8ffe7c879616340
Alex Posada <aposadaentrecanales@gmail.com>
2013-02-12 00:11:23 +0100

    Added sketch to control wifly module using Arduino terminal. Recommended update firmware after connecting to wifly network. Command: "ftp upload" + enter

---
+++ Wifly_terminal/Wifly_terminal.ino
@@ -0,0 +1,25 @@
+void setup() { 
+ //Initialize serial and wait for port to open:
+  #define AWAKE  4 //Wake up WIFI
+  pinMode(AWAKE, OUTPUT);
+  digitalWrite(AWAKE, HIGH); 
+  Serial.begin(9600); 
+  Serial1.begin(9600); 
+  delay(5000);
+  Serial1.print("$$$");
+  delay(250);
+  Serial1.println();
+  Serial1.println();
+} 
+
+void loop() { 
+  if (Serial.available()) 
+  {
+    int inByte = Serial.read();
+    Serial1.write(inByte); 
+  }
+  if (Serial1.available()) {
+    int inByte = Serial1.read();
+    Serial.write(inByte); 
+  }
+} 
