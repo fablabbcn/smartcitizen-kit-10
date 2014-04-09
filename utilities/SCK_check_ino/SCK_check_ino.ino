c2604fc4a9acc805ff8f26acf9347f593e4b2762
mdeheras <mdeheras@gmail.com>
2014-04-09 22:07:55 +0200

    New SCK_check

---
+++ utilities/SCK_check_ino/SCK_check_ino.ino
@@ -0,0 +1,20 @@
+void setup() { 
+  sckBegin();
+  delay(5000);
+  if (!sckRepair()) sckRecovery();
+  else Serial.println("Device detected");
+  Serial.print("Firmware version: "); 
+  Serial.println(getWiFlyVersion());
+} 
+
+void loop() {   
+  if (Serial.available()) 
+  {
+    int inByte = Serial.read();
+    Serial1.write(inByte); 
+  }
+  if (Serial1.available()) {
+    int inByte = Serial1.read();
+    Serial.write(inByte); 
+  }
+} 
