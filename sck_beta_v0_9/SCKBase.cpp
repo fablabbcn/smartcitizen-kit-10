a4de0910d0964d00a6f5348200d6ab2b041a2435
mdeheras <mdeheras@gmail.com>
2014-10-12 20:17:39 +0200

    Revert "fixed bug in autoupdate"
    This reverts commit 623a83500125bb66108c16e2cb8840cebe86df68.

--- sck_beta_v0_9/SCKBase.cpp
+++ sck_beta_v0_9/SCKBase.cpp
@@ -757,9 +757,8 @@ int SCKBase::checkWiFly() {
   {
     if (ver < WIFLY_LATEST_VERSION)
      {
-      byte state = 1;
-      if(update()); //Wifly Updated.
-      else state = 2; //Update Fail.
+      if(update()) return 1; //Wifly Updated.
+      else return 2; //Update Fail.
       reset();
      }   
     else return 0; //WiFly up to date.
